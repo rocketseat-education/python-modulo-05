e28959d9c770d1f5d807a27034e9ed7b748d603b
Luis Fellipe Batista Silva <63753269+offellipe@users.noreply.github.com>
2024-03-13 20:13:19 -0300

    Commit -  Aula 06

---
+++ payments/pix.py
@@ -0,0 +1,22 @@
+import uuid
+import qrcode
+
+
+class Pix:
+
+    def __init__(self) -> None:
+        pass
+
+    def create_payment(self):
+        bank_payment_id = uuid.uuid4()
+
+        hash_payment = f'hash_payment_{bank_payment_id}'
+
+        img = qrcode.make(hash_payment)
+
+        img.save(f"static/img/qr_code_payment_{bank_payment_id}.png")
+
+        return {
+            "bank_payment_id":  bank_payment_id,
+            "qr_code_path": f"qr_code_payment_{bank_payment_id}",
+            }
