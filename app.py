dc639bda203613ede667ffe20fe7c0cce48b8691
Luis Fellipe Batista Silva <63753269+offellipe@users.noreply.github.com>
2024-03-13 19:40:49 -0300

    Commit - Aula 05

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from repository.database import db
 from db_models.payment import Payment
+from datetime import datetime, timedelta
 
 
 app = Flask(__name__)
@@ -13,7 +14,22 @@ db.init_app(app)
 
 @app.route('/payments/pix', methods=['POST'])
 def create_payment_pix():
-    return jsonify({"message": "The payment has been created"})
+    data = request.get_json()
+
+    if 'value' not in data:
+        return jsonify({"message": "invalid value"}), 400
+
+    expiration_date = datetime.now() + timedelta(minutes=30)
+
+    new_payment = Payment(value=data['value'], expiration_date=expiration_date)
+
+    db.session.add(new_payment)
+    db.session.commit()
+
+    return jsonify({
+        "message": "The payment has been created",
+        "payment": new_payment.to_dict(),
+        })
 
 
 @app.route('/payments/pix/confirmation', methods=['POST'])
--- db_models/payment.py
+++ db_models/payment.py
@@ -9,3 +9,13 @@ class Payment(db.Model):
     bank_payment_id = db.Column(db.Integer, nullable=True)
     qr_code = db.Column(db.String(100), nullable=True)
     expiration_date = db.Column(db.DateTime)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "value": self.value,
+            "paid": self.paid,
+            "bank_payment_id": self.bank_payment_id,
+            "qr_code": self.qr_code,
+            "expiration_date": self.expiration_date,
+        }
